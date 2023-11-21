e20b446a2e824d130cb8b08be5fc185af45a5f06
Samvel Yeghikyan <Samvel.Yeghikyan@technamin.com>
2023-11-21 22:34:33 +0400

    single linkedlist

---
+++ DataStracture/LinkedListSingle.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+
+namespace DataStracture
+{
+    public class LinkedListSingle<T> : ICollection<T>
+    {
+        public LinkedListNode<T> head;
+        public LinkedListNode<T> tail;
+
+        public int Count { get; set; }
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            LinkedListNode<T> value = new(item);
+            if (head == null)
+            {
+                head = value;
+                tail = value;
+            }
+            else
+            {
+                tail.Next = value;
+                tail = value;
+            }
+            Count++;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            LinkedListNode<T> current = head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0 || (array.Length < arrayIndex + Count))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            LinkedListNode<T> current = head;
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            LinkedListNode<T> current = head;
+            LinkedListNode<T> previous = null;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    if (previous == null)
+                    {
+                        head = head.Next;
+                        if (head == null)
+                            tail = null;
+                    }
+                    else
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next == null)
+                        {
+                            tail = previous;
+                        }
+                    }
+                    Count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- DataStracture/Program.cs
+++ DataStracture/Program.cs
@@ -4,25 +4,25 @@
     {
         public static void Main()
         {
-            LinkedList<int> list = [1, 5, 52, 10, 17];
+            LinkedListSingle<int> list = [1,3,4,5,7,8,11];
             Display(list, "List");
-            list.Remove(5);
-            Display(list, "Remove 5 ");
-            int[] arr = new int[5];
+            list.Remove(11);
+            Display(list, "Remove 11");
+            int[] arr = new int[9];
             list.CopyTo(arr, 3);
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.WriteLine(arr[i]);
             }
         }
-        public static void Display(LinkedList<int> list, string test)
+        public static void Display(LinkedListSingle<int> list, string test)
         {
             Console.WriteLine(test);
             foreach (int item in list)
             {
                 Console.WriteLine(item + "  ");
             }
-            Console.WriteLine();
+            Console.WriteLine("----////-----");
             Console.WriteLine();
 
         }
