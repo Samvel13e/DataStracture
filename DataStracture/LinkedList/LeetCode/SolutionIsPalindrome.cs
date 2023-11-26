e52aaf7bb2c477e7427ab3af0d0c1b1b8aea003d
samvel <samvel.yeghikyan@am.technamin.com>
2023-11-26 17:11:42 +0400

    Stack

---
+++ DataStracture/LinkedList/LeetCode/SolutionIsPalindrome.cs
@@ -0,0 +1,24 @@
+﻿namespace DataStracture.LinkedList.LeetCode
+{
+    public class SolutionIsPalindrome
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            ListNode current = head;
+            ListNode reversedNode = null;
+            while (current != null)
+            {
+                reversedNode = new ListNode(current.val, reversedNode);
+                current = current.next;
+            }
+
+            while (reversedNode != null)
+            {
+                if (reversedNode.val != head.val) return false;
+                reversedNode = reversedNode.next;
+                head = head.next;
+            }
+            return true;
+        }
+    }
+}
---
+++ DataStracture/Stack/Stack.cs
@@ -0,0 +1,34 @@
+﻿namespace DataStracture.Stack
+{
+    public class Stack<T>
+    {
+        private class Node(T data, Node next)
+        {
+            public T _data = data;
+            public Node _next = next;
+        }
+        private Node top;
+        private int count;
+        public Stack()
+        {
+            top = null;
+            count = 0;
+        }
+        public void Push(T item)
+        {
+            top = new Node(item, top);
+            count++;
+        }
+        public T Pop()
+        {
+            T result = top._data;
+            top = top._next;
+            count--;
+            return result;
+        }
+        public T Peek()
+        {
+            return top._data;
+        }
+    }
+}
